a11616cdb6353297b9a3726055154157b116cce7
Rodrigo Siqueira <rodriados@gmail.com>
2018-08-30 00:28:58 -0400

    Adding cluster.cpp

---
+++ src/cluster.cpp
@@ -0,0 +1,21 @@
+/** 
+ * Multiple Sequence Alignment cluster file.
+ * @author Rodrigo Siqueira <rodriados@gmail.com>
+ * @copyright 2018 Rodrigo Siqueira
+ */
+#include <vector>
+#include <mpi.h>
+
+#include "cluster.hpp"
+
+namespace cluster
+{
+    /*
+     * Declaring global variables.
+     */
+    int rank = 0;
+    int size = 0;
+
+    std::vector<MPI_Datatype> createdTypes;
+};
+
