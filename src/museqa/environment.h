0120918bf164261bdf521f84f20713e89a3bfca8
Rodrigo Siqueira <rodriados@gmail.com>
2021-05-22 02:44:08 -0400

    [utility] Reorganizing utilities to final structure

---
+++ src/museqa/environment.h
@@ -0,0 +1,148 @@
+/**
+ * Museqa: Multiple Sequence Aligner using hybrid parallel computing.
+ * @file Environment configuration and macro values.
+ * @author Rodrigo Siqueira <rodriados@gmail.com>
+ * @copyright 2018-present Rodrigo Siqueira
+ */
+#pragma once
+
+/**
+ * Information about the source code version used to compile the project. The version
+ * allows the developers to identify when an error was introduced to the codebase,
+ * whenever a new bug is reported. Also, it allows the control of feature deprecation
+ * and introduction.
+ */
+#define MUSEQA_VERSION 1000 // The version number is (major * 1000 + minor)
+
+/*
+ * The software's authorship information. If you need to get in touch with this
+ * software's author, please use the info available below.
+ */
+#define MUSEQA_APPNAME "Museqa: Multiple Sequence Aligner using hybrid parallel computing."
+#define MUSEQA_AUTHOR  "Rodrigo Albuquerque de Oliveira Siqueira"
+#define MUSEQA_EMAIL   "rodriados at gmail dot com"
+
+/*
+ * Indicates the target environment mode to which the code must be compiled to.
+ * The mode may affect some features' availability and performance.
+ */
+#if defined(DEBUG)
+  #define MUSEQA_DEBUG "debug"
+  #define MUSEQA_ENV 1
+#elif defined(TESTING)
+  #define MUSEQA_TESTING "testing"
+  #define MUSEQA_ENV 2
+#elif defined(PRODUCTION)
+  #define MUSEQA_PRODUCTION "production"
+  #define MUSEQA_ENV 3
+#else
+  #define MUSEQA_DEV "development"
+  #define MUSEQA_ENV 4
+#endif
+
+/*
+ * Checks the current compiler's C++ language level. As the majority of this software's
+ * codebase is written in C++, we must check whether its available or not.
+ */
+#if !defined(__cplusplus) || __cplusplus < 201103L
+  #error "This software requires a C++11 enabled compiler."
+#else
+  #define MUSEQA_CPP __cplusplus
+#endif
+
+/*
+ * Finds the version of compiler being used. Some features might change or be unavailable
+ * depending on the current compiler configuration.
+ */
+#if defined(__GNUC__)
+  #if !defined(__clang__)
+    #define MUSEQA_COMPILER_GCC (__GNUC__ * 100 + __GNUC_MINOR__)
+    #define MUSEQA_COMPILER "gcc"
+  #else
+    #define MUSEQA_COMPILER_CLANG (__clang_major__ * 100 + __clang_minor__)
+    #define MUSEQA_COMPILER "clang"
+  #endif
+  #define MUSEQA_COMPILER_GNUC 1
+#endif
+
+#if defined(__NVCC__) || defined(__CUDACC__)
+  #define MUSEQA_COMPILER_NVCC (__CUDACC_VER_MAJOR__ * 100 + __CUDACC_VER_MINOR__)
+  #if defined(MUSEQA_COMPILER)
+    #undef MUSEQA_COMPILER
+  #endif
+  #define MUSEQA_COMPILER "nvcc"
+#endif
+
+#if defined(__INTEL_COMPILER) || defined(__ICL)
+  #define MUSEQA_COMPILER_ICC __INTEL_COMPILER
+  #define MUSEQA_COMPILER "icc"
+#endif
+
+#if defined(_MSC_VER)
+  #define MUSEQA_COMPILER_MSC _MSC_VER
+  #define MUSEQA_COMPILER "msc"
+#endif
+
+#if !defined(MUSEQA_COMPILER)
+  #define MUSEQA_COMPILER_UNKNOWN 1
+  #define MUSEQA_COMPILER "unknown"
+#endif
+
+/* 
+ * Discovers about the environment in which the software is being compiled. Some
+ * conditional compiling may take place depending on the environment.
+ */
+#if defined(__unix__) || (defined(__APPLE__) && defined(__MACH__))
+  #if defined(__linux__) || defined(__gnu_linux__)
+    #define MUSEQA_OS_LINUX 1
+    #define MUSEQA_OS "linux"
+  #else
+    #define MUSEQA_OS_APPLE 1
+    #define MUSEQA_OS "apple"
+  #endif
+  #define MUSEQA_OS_UNIX 1
+#elif defined(WIN32) || defined(_WIN32) || defined(__WIN32)
+  #define MUSEQA_OS_WINDOWS 1
+  #define MUSEQA_OS "windows"
+#endif
+
+#if !defined(MUSEQA_OS)
+  #define MUSEQA_OS_UNKNOWN 1
+  #define MUSEQA_OS "unknown"
+#endif
+
+/*
+ * Checks whether the current compiler is compatible with the software's prerequisites.
+ * Should it not be compatible, then we emit a warning but try compiling anyway.
+ */
+#if defined(MUSEQA_COMPILER_UNKNOWN)
+  #if defined(MUSEQA_PRODUCTION)
+    #warning "This software has not been tested with the current compiler."
+  #endif
+#else
+  #define MUSEQA_COMPILER_TESTED 1
+#endif
+
+/*
+ * Checks whether the software has been tested in the current compilation environment.
+ * Were it not tested, we emit a warning but try compiling anyway.
+ */
+#if defined(MUSEQA_OS_UNKNOWN)
+  #if defined(MUSEQA_PRODUCTION)
+    #warning "This software has not been tested under the current environment."
+  #endif
+#else
+  #define MUSEQA_OS_TESTED 1
+#endif
+
+/*
+ * Discovers about the runtime environment. As this software uses GPUs to run calculations
+ * on, we need to know at times whether the code is being executed in CPU or GPU.
+ */
+#if defined(MUSEQA_COMPILER_NVCC) && defined(__CUDA_ARCH__)
+  #define MUSEQA_RUNTIME_DEVICE 1
+  #define MUSEQA_RUNTIME (0x01)
+#else
+  #define MUSEQA_RUNTIME_HOST 1
+  #define MUSEQA_RUNTIME (0x10)
+#endif
---
+++ src/museqa/utility.hpp
@@ -0,0 +1,143 @@
+/** 
+ * Museqa: Multiple Sequence Aligner using hybrid parallel computing.
+ * @file Miscellaneous utilities and helper functions.
+ * @author Rodrigo Siqueira <rodriados@gmail.com>
+ * @copyright 2018-present Rodrigo Siqueira
+ */
+#pragma once
+
+#include <string>
+#include <cstdint>
+#include <utility>
+
+#include <museqa/environment.h>
+
+/*
+ * Definition of CUDA function flags for host code, so we don't need to care about
+ * which compiler is working on the file when using these flags.
+ */
+#if !defined(__host__) && !defined(__device__)
+  #define __host__
+  #define __device__
+#endif
+
+namespace museqa
+{
+    /**
+     * A general memory storage container.
+     * @tparam S The number of bytes in storage.
+     * @tparam A The byte alignment the storage should use.
+     * @since 1.0
+     */
+    template <size_t S, size_t A = S>
+    struct alignas(A) storage
+    {
+        alignas(A) char storage[S];
+    };
+
+    /**
+     * A general range container.
+     * @tparam T The range's elements type.
+     * @since 1.0
+     */
+    template <typename T = int>
+    struct range
+    {
+        T offset, total;
+    };
+
+    /**
+     * Purifies the type to its base, removing all extents it might have.
+     * @tparam T The type to be purified.
+     * @since 1.0
+     */
+    template <typename T>
+    using pure = typename std::conditional<
+            !std::is_array<T>::value || std::extent<T>::value
+          , typename std::remove_reference<T>::type
+          , typename std::remove_extent<T>::type
+        >::type;
+
+    /**
+     * Returns the type unchanged. This is useful to produce a repeating list of the
+     * given type parameter.
+     * @tpatam T The identity type.
+     * @since 1.0
+     */
+    template <typename T, size_t = 0>
+    using identity = T;
+
+    namespace utility
+    {
+        /**
+         * Parses the given string value into any other generic type.
+         * @tparam T The target type to parse the string to.
+         * @param value The string to be parsed.
+         * @return The parsed value to the requested type.
+         */
+        template <typename T>
+        inline auto parse(const std::string& value)
+        -> typename std::enable_if<std::is_convertible<std::string, T>::value, T>::type
+        {
+            return T (value);
+        }
+
+        /**
+         * Parses the given string value into an integer type.
+         * @tparam T The target type to parse the string to.
+         * @param value The string to be parsed.
+         * @return The parsed value to the requested type.
+         * @throw std::exception Error detected during operation.
+         */
+        template <typename T>
+        inline auto parse(const std::string& value)
+        -> typename std::enable_if<std::is_integral<T>::value, T>::type
+        {
+            return static_cast<T>(std::stoull(value));
+        }
+
+        /**
+         * Parses the given string value into a floating-point type.
+         * @tparam T The target type to parse the string to.
+         * @param value The string to be parsed.
+         * @return The parsed value to the requested type.
+         * @throw std::exception Error detected during operation.
+         */
+        template <typename T>
+        inline auto parse(const std::string& value)
+        -> typename std::enable_if<std::is_floating_point<T>::value, T>::type
+        {
+            return static_cast<T>(std::stold(value));
+        }
+
+        /**
+         * Swaps the contents of two variables of same type
+         * @tparam T The variables' type.
+         * @param a The first variable to have its contents swapped.
+         * @param b The second variable to have its contents swapped.
+         */
+        template <typename T>
+        __host__ __device__ inline void swap(T& a, T& b) noexcept(
+            std::is_nothrow_move_constructible<T>::value &&
+            std::is_nothrow_move_assignable<T>::value
+        ) {
+            T x = std::move(a);
+              a = std::move(b);
+              b = std::move(x);
+        }
+
+        /**
+         * Swaps the elements of two arrays of same type and size.
+         * @tparam T The arrays' elements' swappable type.
+         * @tparam N The arrays' size.
+         * @param a The first array to have its elements swapped.
+         * @param b The second array to have its elements swapped.
+         */
+        template <typename T, size_t N>
+        __host__ __device__ inline void swap(T (&a)[N], T (&b)[N]) noexcept(noexcept(swap(*a, *b)))
+        {
+            for(size_t i = 0; i < N; ++i)
+                swap(a[i], b[i]);
+        }
+    }
+}
