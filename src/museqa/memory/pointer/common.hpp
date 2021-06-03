f7eaf6554a73f7d5b567e992cbf8b0ec92af8b7a
Rodrigo Siqueira <rodriados@gmail.com>
2021-06-03 03:36:50 -0400

    [pointer] Reimplementing GPU-capable shared pointers

---
+++ src/museqa/memory/pointer/common.hpp
@@ -0,0 +1,290 @@
+/**
+ * Museqa: Multiple Sequence Aligner using hybrid parallel computing.
+ * @file The common interface for an automatic pointer implementation.
+ * @author Rodrigo Siqueira <rodriados@gmail.com>
+ * @copyright 2021-present Rodrigo Siqueira
+ */
+#pragma once
+
+#include <cstddef>
+#include <utility>
+
+#include <museqa/environment.h>
+
+#include <museqa/utility.hpp>
+#include <museqa/memory/allocator.hpp>
+
+namespace museqa
+{
+    namespace memory
+    {
+        namespace impl
+        {
+            /**
+             * Keeps track of the metadata and the number of references to a given
+             * memory pointer. Whenever the number of references to a pointer reaches
+             * zero, it is automatically deallocated.
+             * @since 1.0
+             */
+            class metadata
+            {
+              private:
+                typedef memory::allocator allocator_type;
+
+              private:
+                void *m_ptr = nullptr;                          /// The raw wrapped pointer.
+                size_t m_counter = 0;                           /// The number of pointer instances.
+                const allocator_type m_allocator;               /// The pointer's allocator.
+
+              public:
+                /**
+                 * Creates a new pointer context from given arguments.
+                 * @param ptr The pointer to be wrapped into the context.
+                 * @param allocator The pointer's allocator.
+                 * @return The new pointer's metadata instance.
+                 */
+                inline static auto acquire(void *ptr, const allocator_type& allocator) noexcept -> metadata *
+                {
+                    return new metadata {ptr, allocator};
+                }
+
+                /**
+                 * Acquires ownership of an already existing pointer.
+                 * @param meta The metadata of pointer to be acquired.
+                 * @return The acquired metadata pointer.
+                 */
+                __host__ __device__ inline static auto acquire(metadata *meta) noexcept -> metadata *
+                {
+                    #if defined(MUSEQA_RUNTIME_HOST)
+                        if (nullptr != meta) { ++meta->m_counter; }
+                        return meta;
+                    #endif
+                }
+
+                /**
+                 * Releases ownership of a pointer, and deletes it if needed.
+                 * @param meta The metadata of pointer to be released.
+                 */
+                __host__ __device__ inline static void release(metadata *meta)
+                {
+                    #if defined(MUSEQA_RUNTIME_HOST)
+                        if (nullptr != meta && --meta->m_counter <= 0) { delete meta; }
+                    #endif
+                }
+
+              private:
+                /**
+                 * Initializes a new pointer metadata.
+                 * @param ptr The raw pointer to be handled.
+                 * @param allocator The pointer's allocator.
+                 */
+                inline explicit metadata(void *ptr, const allocator_type& allocator) noexcept
+                  : m_ptr {ptr}
+                  , m_counter {1}
+                  , m_allocator {allocator}
+                {}
+
+                /**
+                 * Deletes and frees the memory occupied by the raw pointer.
+                 * @see museqa::memory::pointer::impl::metadata::release
+                 */
+                inline ~metadata()
+                {
+                    if (nullptr != m_ptr) { m_allocator.deallocate(m_ptr); }
+                }
+            };
+
+            /**
+             * Wraps a generic pointer into an owning container.
+             * @param T The pointer's concrete type.
+             * @since 1.0
+             */
+            template <typename T>
+            class pointer
+            {
+                static_assert(!std::is_function<T>(), "cannot create pointer to a function");
+                static_assert(!std::is_reference<T>(), "cannot create pointer to a reference");
+                static_assert(!std::is_member_pointer<T>(), "cannot create pointer to a member");
+                static_assert(!std::is_member_object_pointer<T>(), "cannot create pointer to an object member");
+                static_assert(!std::is_member_function_pointer<T>(), "cannot create pointer to a function member");
+
+              public:
+                typedef pure<T> element_type;                   /// The pointer's contents type.
+
+              protected:
+                element_type *m_ptr = nullptr;                  /// The raw encapsulated pointer.
+
+              protected:
+                __host__ __device__ inline constexpr pointer() noexcept = default;
+                __host__ __device__ inline constexpr pointer(const pointer&) noexcept = default;
+                __host__ __device__ inline constexpr pointer(pointer&&) noexcept = default;
+
+                /**
+                 * Builds a new instance from a raw pointer.
+                 * @param ptr The pointer to be encapsulated.
+                 */
+                inline pointer(element_type *ptr) noexcept
+                  : m_ptr {ptr}
+                {}
+
+                __host__ __device__ inline pointer& operator=(const pointer&) noexcept = default;
+                __host__ __device__ inline pointer& operator=(pointer&&) noexcept = default;
+
+              public:
+                /**
+                 * The pointer dereferencing operator. This operator exposes the
+                 * internally owned object via a reference.
+                 * @return A reference to the owned object.
+                 */
+                __host__ __device__ inline element_type& operator*() const noexcept
+                {
+                    return *m_ptr;
+                }
+
+                /**
+                 * The member access operator. This operator exposes the internally
+                 * owned pointer for a member access.
+                 * @return The owned pointer.
+                 */
+                __host__ __device__ inline element_type *operator->() const noexcept
+                {
+                    return m_ptr;
+                }
+
+                /**
+                 * Converts the pointer wrapper into the element type pointer.
+                 * @return The internally owned pointer.
+                 */
+                __host__ __device__ inline explicit operator element_type*() const noexcept
+                {
+                    return m_ptr;
+                }
+
+                /**
+                 * Checks whether the stored pointer is null or not.
+                 * @return Is the pointer not null?
+                 */
+                __host__ __device__ inline explicit operator bool() const noexcept
+                {
+                    return (nullptr != m_ptr);
+                }
+
+                /**
+                 * Gives access to the raw owned pointer.
+                 * @return The raw encapsulated pointer.
+                 */
+                __host__ __device__ inline element_type *get() const noexcept
+                {
+                    return m_ptr;
+                }
+            };
+
+            /**
+             * Wraps a type-erased pointer into an owning container.
+             * @since 1.0
+             */
+            template <>
+            class pointer<void>
+            {
+              protected:
+                void *m_ptr = nullptr;                          /// The raw encapsulated pointer.
+
+              protected:
+                __host__ __device__ inline constexpr pointer() noexcept = default;
+                __host__ __device__ inline constexpr pointer(const pointer&) noexcept = default;
+                __host__ __device__ inline constexpr pointer(pointer&&) noexcept = default;
+
+                /**
+                 * Builds a new instance from a raw generic pointer.
+                 * @param ptr The pointer to be encapsulated.
+                 */
+                inline pointer(void *ptr) noexcept
+                  : m_ptr {ptr}
+                {}
+
+                __host__ __device__ inline pointer& operator=(const pointer&) noexcept = default;
+                __host__ __device__ inline pointer& operator=(pointer&&) noexcept = default;
+
+              public:
+                /**
+                 * Reinterprets the owned pointer to a dereferenceable pointer.
+                 * @tparam T The type to reinterpret the pointer to.
+                 * @return The reinterpreted owned pointer.
+                 */
+                template <typename T>
+                __host__ __device__ inline explicit operator T*() const noexcept
+                {
+                    return static_cast<T*>(m_ptr);
+                }
+
+                /**
+                 * Checks whether the stored pointer is null or not.
+                 * @return Is the pointer not null?
+                 */
+                __host__ __device__ inline explicit operator bool() const noexcept
+                {
+                    return (nullptr != m_ptr);
+                }
+
+                /**
+                 * Gives access to the raw owned pointer.
+                 * @return The raw encapsulated pointer.
+                 */
+                __host__ __device__ inline void *get() const noexcept
+                {
+                    return m_ptr;
+                }
+            };
+
+            /**
+             * Wraps a generic array into an owning container.
+             * @param T The array pointer's concrete type.
+             * @since 1.0
+             */
+            template <typename T>
+            class pointer<T[]> : public pointer<T>
+            {
+              private:
+                typedef pointer<T> underlying_type;             /// The underlying pointer type.
+
+              public:
+                using typename underlying_type::element_type;   /// The array's contents type.
+
+              protected:
+                using underlying_type::pointer;
+                using underlying_type::operator=;
+
+              public:
+                /**
+                 * Gives access to an element in the array from its offset.
+                 * @param offset The requested element offset to be retrieved.
+                 * @return The requested array's element reference.
+                 */
+                __host__ __device__ inline element_type& operator[](ptrdiff_t offset) const noexcept
+                {
+                    return *(this->m_ptr + offset);
+                }
+            };
+
+            /**
+             * Wraps a fixed-size array into an owning container.
+             * @param T The array pointer's concrete type.
+             * @param N The array's fixed size.
+             * @since 1.0
+             */
+            template <typename T, size_t N>
+            class pointer<T[N]> : public pointer<T[]>
+            {
+              private:
+                typedef pointer<T[]> underlying_type;           /// The underlying pointer type.
+
+              public:
+                using typename underlying_type::element_type;   /// The array's contents type.
+
+              protected:
+                using underlying_type::pointer;
+                using underlying_type::operator=;
+            };
+        }
+    }
+}
---
+++ src/museqa/memory/pointer/shared.hpp
@@ -0,0 +1,202 @@
+/**
+ * Museqa: Multiple Sequence Aligner using hybrid parallel computing.
+ * @file Implements generic CUDA-compatible shared pointers.
+ * @author Rodrigo Siqueira <rodriados@gmail.com>
+ * @copyright 2018-present Rodrigo Siqueira
+ */
+#pragma once
+
+#include <cstddef>
+#include <utility>
+
+#include <museqa/utility.hpp>
+#include <museqa/memory/allocator.hpp>
+#include <museqa/memory/pointer/common.hpp>
+
+namespace museqa
+{
+    namespace memory
+    {
+        namespace pointer
+        {
+            /**
+             * Implements a shared pointer. This object can be used to represent
+             * a pointer that is managed and deleted automatically when all references
+             * to it have been destroyed.
+             * @tparam T The type of pointer to be held.
+             * @since 1.0
+             */
+            template <typename T>
+            class shared : public impl::pointer<T>
+            {
+              private:
+                typedef impl::pointer<T> underlying_type;
+                typedef impl::metadata metadata_type;
+
+              public:
+                using typename underlying_type::element_type;
+                using allocator_type = memory::allocator;
+
+              private:
+                metadata_type *m_meta = nullptr;                /// The pointer's metadata object instance.
+
+              public:
+                __host__ __device__ inline constexpr shared() noexcept = default;
+
+                /**
+                 * Builds a new instance from a raw pointer.
+                 * @param ptr The pointer to be encapsulated.
+                 */
+                inline explicit shared(element_type *ptr) noexcept
+                  : shared {ptr, factory::allocator<element_type>()}
+                {}
+
+                /**
+                 * Builds a new instance from a raw pointer and its allocator.
+                 * @param ptr The pointer to be encapsulated.
+                 * @param allocator The allocator of given pointer.
+                 */
+                inline shared(element_type *ptr, const allocator_type& allocator) noexcept
+                  : shared {ptr, metadata_type::acquire(ptr, allocator)}
+                {}
+
+                /**
+                 * The shared pointer's copy constructor.
+                 * @param other The instance to be copied.
+                 */
+                __host__ __device__ inline shared(const shared& other) noexcept
+                  : shared {other.m_ptr, metadata_type::acquire(other.m_meta)}
+                {}
+
+                /**
+                 * The copy constructor from a foreign pointer type.
+                 * @tparam U The foreign pointer type to be copied.
+                 * @param other The foreign instance to be copied.
+                 */
+                template <typename U>
+                __host__ __device__ inline shared(const shared<U>& other) noexcept
+                 : shared {static_cast<element_type *>(other.m_ptr), metadata_type::acquire(other.m_meta)}
+                {}
+
+                /**
+                 * The shared pointer's move constructor.
+                 * @param other The instance to be moved.
+                 */
+                __host__ __device__ inline shared(shared&& other) noexcept
+                {
+                    other.swap(*this);
+                }
+
+                /**
+                 * The move constructor from a foreign pointer type.
+                 * @tparam U The foreign pointer type to be moved.
+                 * @param other The foreign instance to be moved.
+                 */
+                template <typename U>
+                __host__ __device__ inline shared(shared<U>&& other) noexcept
+                  : shared {static_cast<element_type *>(other.m_ptr), metadata_type::acquire(other.m_meta)}
+                {
+                    other.reset();
+                }
+
+                /**
+                 * Releases the ownership of the acquired pointer reference.
+                 * @see shared::shared
+                 */
+                __host__ __device__ inline ~shared()
+                {
+                    metadata_type::release(m_meta);
+                }
+
+                /**
+                 * The copy-assignment operator.
+                 * @param other The instance to be copied.
+                 * @return This pointer object.
+                 */
+                __host__ __device__ inline shared& operator=(const shared& other)
+                {
+                    metadata_type::release(m_meta);
+                    return *new (this) shared {other};
+                }
+
+                /**
+                 * The copy-assignment operator from a foreign pointer type.
+                 * @tparam U The foreign pointer type to be copied.
+                 * @param other The foreign instance to be copied.
+                 * @return This pointer object.
+                 */
+                template <typename U>
+                __host__ __device__ inline shared& operator=(const shared<U>& other)
+                {
+                    metadata_type::release(m_meta);
+                    return *new (this) shared {other};
+                }
+
+                /**
+                 * The move-assignment operator.
+                 * @param other The instance to be moved.
+                 * @return This pointer object.
+                 */
+                __host__ __device__ inline shared& operator=(shared&& other)
+                {
+                    metadata_type::release(m_meta);
+                    return *new (this) shared {std::forward<decltype(other)>(other)};
+                }
+
+                /**
+                 * The move-assignment operator from a foreign pointer type.
+                 * @tparam U The foreign pointer type to be moved.
+                 * @param other The foreign instance to be moved.
+                 * @return This pointer object.
+                 */
+                template <typename U>
+                __host__ __device__ inline shared& operator=(shared<U>&& other)
+                {
+                    metadata_type::release(m_meta);
+                    return *new (this) shared {std::forward<decltype(other)>(other)};
+                }
+
+                /**
+                 * Creates an instance to an offset of the wrapped pointer.
+                 * @param offset The requested offset.
+                 * @return The new offset pointer instance.
+                 */
+                __host__ __device__ inline shared offset(ptrdiff_t offset) noexcept
+                {
+                    return shared {this->m_ptr + offset, metadata_type::acquire(m_meta)};
+                }
+
+                /**
+                 * Releases the pointer ownership and returns to an empty state.
+                 * @see shared::shared
+                 */
+                __host__ __device__ inline void reset() noexcept
+                {
+                    metadata_type::release(m_meta);
+                    new (this) shared {};
+                }
+
+                /**
+                 * Swaps ownership with another pointer instance.
+                 * @param other The instance to swap with.
+                 */
+                __host__ __device__ inline void swap(shared& other) noexcept
+                {
+                    utility::swap(this->m_ptr, other.m_ptr);
+                    utility::swap(m_meta, other.m_meta);
+                }
+
+              private:
+                /**
+                 * Builds a new instance from a raw pointer and its metadata.
+                 * @param ptr The raw pointer object.
+                 * @param meta The pointer's metadata.
+                 */
+                __host__ __device__ inline explicit shared(element_type *ptr, metadata_type *meta) noexcept
+                  : underlying_type {ptr}
+                  , m_meta {meta}
+                {}
+            };
+        }
+    }
+}
