f464e7933b567a699d02a7507c01a95c52c64e54
Rodrigo Siqueira <rodriados@gmail.com>
2022-10-04 03:44:09 -0400

    [config] Allowing namespace to be overriden if necessary

--- src/museqa/config/namespace.hpp
+++ src/museqa/config/namespace.hpp
@@ -7,20 +7,32 @@
 #pragma once
 
 /**
+ * Defines the namespace in which the library lives. This might be overriden if
+ * the default namespace value is already in use.
+ * @since 1.0
+ */
+#if defined(MUSEQA_OVERRIDE_NAMESPACE)
+  #define MUSEQA_NAMESPACE MUSEQA_OVERRIDE_NAMESPACE
+#else
+  #define MUSEQA_NAMESPACE museqa
+#endif
+
+/**
  * This macro is used to open the `museqa::` namespace block and must not be in
  * any way overriden. This namespace must not be prefixed by any other namespaces
  * to avoid problems when allowing the use some of the library's facilities to with
  * STL's algorithms, structures or constructions.
  * @since 1.0
  */
-#define MUSEQA_BEGIN_NAMESPACE                                                  \
-    namespace museqa {                                                          \
-    inline namespace v1 {
+#define MUSEQA_BEGIN_NAMESPACE   \
+    namespace MUSEQA_NAMESPACE { \
+        inline namespace v1 {    \
+            namespace museqa = MUSEQA_NAMESPACE;
 
 /**
  * This macro is used to close the `museqa::` namespace block and must not be in
  * any way overriden.
  * @since 1.0
  */
-#define MUSEQA_END_NAMESPACE                                                    \
+#define MUSEQA_END_NAMESPACE     \
     }}
