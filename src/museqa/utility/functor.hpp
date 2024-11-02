edc39852742f432baa433100ad14b3c6550dcf76
Rodrigo Siqueira <rodriados@gmail.com>
2024-11-02 01:48:42 -0400

    [functor] Simplify function & remove delegate

--- src/museqa/utility/functor.hpp
+++ src/museqa/utility/functor.hpp
@@ -10,265 +10,181 @@
 
 #include <museqa/environment.h>
 #include <museqa/guard.hpp>
-
 #include <museqa/utility.hpp>
-#include <museqa/utility/delegate.hpp>
-#include <museqa/memory/pointer/shared.hpp>
-#include <museqa/memory/pointer/unmanaged.hpp>
-
-#include <museqa/thirdparty/supertuple.h>
 
 MUSEQA_BEGIN_NAMESPACE
 
 namespace utility
 {
     /**
-     * A functor wrapper for bound or unbound functions. The wrapped function might
-     * have any function type, including as a member function pointer. If the wrapped
-     * function has member function type, an instance of the object to which the
-     * function is bound to is necessary to create a functor.
+     * A functor wrapper for bounded or unbounded functions. The wrapped function
+     * might have any function type, including a member function pointer. If the
+     * wrapped function is of a member function type, an instance of the object
+     * to which the function is bounded must be its first parameter when called.
      * @tparam F The wrapped function pointer type.
-     * @tparam O The object type to which the function might be bound to.
+     * @tparam O The object type to which the function might be bounded to.
      * @since 1.0
      */
     template <typename F, typename O = void>
     class functor_t;
 
-    /**
-     * Wraps a generic non-member function, callable from any context.
-     * @tparam F The wrapped function pointer type.
-     * @since 1.0
-     */
-    template <typename F>
-    class functor_t<F, void> : public decltype(utility::delegate_t(std::declval<F>()))
-    {
-        private:
-            typedef decltype(utility::delegate_t(std::declval<F>())) underlying_t;
-
-        public:
-            using underlying_t::delegate_t;
-
-            /**
-             * Unwraps and exposes the underlying function pointer.
-             * @return The unwrapped function pointer.
-             */
-            __host__ __device__ inline constexpr F *unwrap() const
-            {
-                return this->m_function;
-            }
-    };
-
     namespace detail
     {
         /**
-         * Analyses whether the given type is a member function pointer and if so,
-         * isolates the member function's return type.
-         * @tparam F The member function type to be analysed.
+         * Extracts the return type of a generic function type.
+         * @tparam F The function type to be analyzed.
          * @since 1.0
          */
         template <typename F>
-        struct analyzer_t : std::false_type {};
+        struct function_return_t;
 
         /*
-         * Defines auxiliary macros for creating all possible member function types.
+         * Auxiliary macros for creating every possible function type with qualifiers.
          * Although quite repetitive, there's apparently no better way to do it.
          */
-        #define __museqaanalyzer__(MF)                                         \
-          template <typename R, typename T, typename ...P>                     \
-          struct analyzer_t<MF> : std::true_type { typedef R result_t; };
+        #define MUSEQA_DECLARE_FN_RESULT_T(F)                   \
+          template <typename R, typename ...P>                  \
+          struct function_return_t<F> : identity_t<R> {};
 
-        #define __museqarepeater1__(qq)                                        \
-          __museqaanalyzer__(R(T::*)(P...) qq)                                 \
-          __museqaanalyzer__(R(T::*)(P......) qq)
+        #define MUSEQA_DECLARE_FN_RESULT_T_VARIADIC(Q)          \
+          MUSEQA_DECLARE_FN_RESULT_T(R(P...) Q)                 \
+          MUSEQA_DECLARE_FN_RESULT_T(R(P..., ...) Q)
 
-        #define __museqarepeater2__(qq)                                        \
-          __museqarepeater1__(qq)                                              \
-          __museqarepeater1__(const qq)                                        \
-          __museqarepeater1__(volatile qq)                                     \
-          __museqarepeater1__(const volatile qq)
+        #define MUSEQA_DECLARE_FN_RESULT_T_QUALIFIER(E)         \
+          MUSEQA_DECLARE_FN_RESULT_T_VARIADIC(E)                \
+          MUSEQA_DECLARE_FN_RESULT_T_VARIADIC(const E)          \
+          MUSEQA_DECLARE_FN_RESULT_T_VARIADIC(volatile E)       \
+          MUSEQA_DECLARE_FN_RESULT_T_VARIADIC(const volatile E)
 
         /*
-         * Creates every possible combination of qualifiers for a member function
-         * type. Therefore, the return type of any member function can be now known.
+         * Creates every possible combination of qualifiers for generic function types.
+         * Therefore, the return type of any function can be now known.
          */
-        __museqarepeater2__()
-        __museqarepeater2__(&)
-        __museqarepeater2__(&&)
-        __museqarepeater2__(noexcept)
-        __museqarepeater2__(& noexcept)
-        __museqarepeater2__(&& noexcept)
+        MUSEQA_DECLARE_FN_RESULT_T_QUALIFIER()
+        MUSEQA_DECLARE_FN_RESULT_T_QUALIFIER(&)
+        MUSEQA_DECLARE_FN_RESULT_T_QUALIFIER(&&)
+        MUSEQA_DECLARE_FN_RESULT_T_QUALIFIER(noexcept)
+        MUSEQA_DECLARE_FN_RESULT_T_QUALIFIER(& noexcept)
+        MUSEQA_DECLARE_FN_RESULT_T_QUALIFIER(&& noexcept)
 
-        #undef __museqarepeater2__
-        #undef __museqarepeater1__
-        #undef __museqaanalyzer__
+        #undef MUSEQA_DECLARE_FN_RESULT_T_QUALIFIER
+        #undef MUSEQA_DECLARE_FN_RESULT_T_VARIADIC
+        #undef MUSEQA_DECLARE_FN_RESULT_T
+
+        /**
+         * Creates the type for a pointer to a bounded function.
+         * @tparam F The function type to get a bounded pointer to.
+         * @tparam O The object type to bind the function to.
+         * @return The bounded function pointer type.
+         */
+        template <
+            typename F
+          , typename O
+          , typename = std::enable_if_t<std::is_function_v<F> && !std::is_void_v<O>>>
+        MUSEQA_CUDA_CONSTEXPR auto make_function_pointer() -> F O::*;
+
+        /**
+         * Creates the type for a pointer to an unbounded function.
+         * @tparam F The function type to get a pointer to.
+         * @tparam O Should be void for unbounded functions.
+         * @return The unbounded function pointer type.
+         */
+        template <
+            typename F
+          , typename O
+          , typename = std::enable_if_t<std::is_function_v<F> && std::is_void_v<O>>>
+        MUSEQA_CUDA_CONSTEXPR auto make_function_pointer() -> F*;
     }
 
     /**
-     * Wraps a generic member function bound to an object instance.
-     * @tparam F The wrapped member function pointer type.
-     * @tparam O The object type to which the function is bound to.
+     * A function wrapper for bounded or unbouded functions.
+     * @tparam F The wrapped function pointer type.
+     * @tparam O The object type to which the function might be bounded to.
      * @since 1.0
      */
     template <typename F, typename O>
     class functor_t
     {
-        static_assert(std::is_class<O>::value, "functor must be bound to a class-like type");
-        static_assert(detail::analyzer_t<F>::value, "only member functions can be bound to type");
-
-        protected:
-            typedef O object_t;
-            typedef memory::pointer::shared_t<O> pointer_t;
+        static_assert(std::is_function_v<F>, "only functions are functors");
+        static_assert(std::is_class_v<O> || std::is_void_v<O>
+          , "a functor must be either unbounded or bounded to an object type");
 
         public:
-            typedef F function_t;
-            typedef typename detail::analyzer_t<F>::result_t result_t;
+            using function_t = F;
+            using bound_object_t = O;
+            using result_t = typename detail::function_return_t<F>::type;
 
         private:
-            mutable pointer_t m_object;
-            function_t m_function = nullptr;
+            using function_pointer_t = decltype(detail::make_function_pointer<F, O>());
+
+        private:
+            function_pointer_t m_function = nullptr;
 
         public:
-            __host__ __device__ inline constexpr functor_t() noexcept = default;
-            __host__ __device__ inline functor_t(const functor_t&) noexcept = default;
-            __host__ __device__ inline functor_t(functor_t&&) noexcept = default;
+            MUSEQA_CONSTEXPR functor_t() noexcept = default;
+            MUSEQA_CONSTEXPR functor_t(const functor_t&) noexcept = default;
+            MUSEQA_CONSTEXPR functor_t(functor_t&&) noexcept = default;
 
             /**
-             * Builds a function from a non-owning pointer to the bound object.
-             * @param object The object to which the member function is bound to.
-             * @param lambda The function pointer to be wrapped by the functor.
+             * Initializes a functor from a raw function or member function pointer.
+             * @param function An invokable function pointer.
              */
-            __host__ __device__ inline explicit functor_t(object_t& object, function_t lambda) noexcept
-              : functor_t (memory::pointer::unmanaged_t(&object), lambda)
+            MUSEQA_CUDA_CONSTEXPR functor_t(function_pointer_t function) noexcept
+              : m_function (function)
             {}
 
-            /**
-             * Builds a functor by sharing ownership of the bound object.
-             * @param object The pointer to the object the function is bound to.
-             * @param lambda The function pointer to be wrapped by the functor.
-             */
-            __host__ __device__ inline functor_t(const pointer_t& object, function_t lambda) noexcept
-              : m_object (object)
-              , m_function (lambda)
-            {}
+            MUSEQA_INLINE functor_t& operator=(const functor_t&) noexcept = default;
+            MUSEQA_INLINE functor_t& operator=(functor_t&&) noexcept = default;
 
             /**
-             * Builds a functor by acquiring ownership of the bound object.
-             * @param object The pointer to the object the function is bound to.
-             * @param lambda The function pointer to be wrapped by the functor.
-             */
-            __host__ __device__ inline functor_t(pointer_t&& object, function_t lambda) noexcept
-              : m_object (std::forward<decltype(object)>(object))
-              , m_function (lambda)
-            {}
-
-            __host__ __device__ inline functor_t& operator=(const functor_t&) __devicesafe__ = default;
-            __host__ __device__ inline functor_t& operator=(functor_t&&) __devicesafe__ = default;
-
-            /**
-             * Invokes the wrapped function and returns the produced result.
-             * @tparam P The given parameters' types.
-             * @param param The parameters' to invoke the functor with.
+             * Invokes the wrapped functor and returns the produced result.
+             * @tparam P The invocation parameters' types.
+             * @param params The parameters' to invoke the functor with.
              * @return The functor execution resulting value.
              */
             template <typename ...P>
-            __host__ __device__ inline decltype(auto) operator()(P&&... param) const
+            MUSEQA_CUDA_CONSTEXPR decltype(auto) operator()(P&&... params) const
             {
-                museqa::guard(!empty(), "an empty functor cannot be invoked");
-                return ((*m_object).*m_function)(std::forward<decltype(param)>(param)...);
+                guard(!empty(), "cannot invoke empty functor");
+                return utility::invoke(m_function, std::forward<decltype(params)>(params)...);
             }
 
             /**
-             * Unwraps and exposes the underlying function pointer and bound object.
-             * @return The unwrapped function pointer and bound object tuple.
+             * Implicitly unwraps the underlying function pointer.
+             * @return The unwrapped function pointer.
              */
-            __host__ __device__ inline constexpr decltype(auto) unwrap() const
+            MUSEQA_CUDA_CONSTEXPR operator function_pointer_t() const
             {
-                return supertuple::tuple_t<object_t&, function_t> {*m_object, m_function};
+                return unwrap();
+            }
+
+            /**
+             * Unwraps the underlying function pointer.
+             * @return The unwrapped function pointer.
+             */
+            MUSEQA_CUDA_CONSTEXPR function_pointer_t unwrap() const noexcept
+            {
+                return m_function;
             }
 
             /**
              * Checks whether the functor is callable or not.
              * @return Is the functor callable?
              */
-            __host__ __device__ inline constexpr bool empty() const noexcept
+            MUSEQA_CUDA_CONSTEXPR bool empty() const noexcept
             {
-                return !m_object || (m_function == nullptr);
+                return m_function == nullptr;
             }
     };
-}
 
-namespace factory
-{
-    /**
-     * Creates a new functor from a plain unbound function.
-     * @tparam F The given function type.
-     * @param function The function to be wrapped.
-     * @return The new functor instance.
+    /*
+     * Deduction guides for generic functors. These guides are responsible for correctly
+     * identifying whether the function is bounded or not.
+     * @since 1.0
      */
-    template <typename F>
-    __host__ __device__ inline constexpr auto functor(const F& function) noexcept
-    -> typename std::enable_if<std::is_function<F>::value, museqa::utility::functor_t<F>>::type
-    {
-        return {function};
-    }
-
-  #if !MUSEQA_RUNTIME_DEVICE
-    /**
-     * Creates a new functor from a method bound to an object.
-     * @tparam T The object type the method is bound to.
-     * @tparam F The given method type.
-     * @param object The object instance the functor is bound to.
-     * @param method The method to be wrapped by functor.
-     * @return The new functor instance.
-     */
-    template <typename T, typename F>
-    __host__ inline auto functor(T&& object, const F& method) -> museqa::utility::functor_t<F, pure_t<T>>
-    {
-        return museqa::utility::functor_t<F, pure_t<T>>(
-            museqa::memory::pointer::shared_t<pure_t<T>>(
-                new pure_t<T>(std::forward<decltype(object)>(object))
-              , [](void *ptr) { delete reinterpret_cast<pure_t<T>*>(ptr); })
-          , method
-        );
-    }
-
-  #else
-    /**
-     * Creates a new functor from a method bound to an object.
-     * @tparam T The object type the method is bound to.
-     * @tparam F The given method type.
-     * @param object The object instance the functor is bound to.
-     * @param method The method to be wrapped by functor.
-     * @return The new functor instance.
-     */
-    template <typename T, typename F>
-    __device__ inline auto functor(T&& object, const F& method) -> museqa::utility::functor_t<F, pure_t<T>>
-    {
-        return museqa::utility::functor_t<F, pure_t<T>>(
-            museqa::memory::pointer::unmanaged_t<pure_t<T>>(&object)
-          , method
-        );
-    }
-  #endif
-
-    /**
-     * Creates a new functor from an invokable object, such as lambdas.
-     * @tparam T The given invokable object type.
-     * @param object The invokable object instance to be wrapped.
-     * @return The new functor instance.
-     */
-    template <typename T>
-    __host__ __device__ inline auto functor(T&& object)
-    -> typename std::enable_if<
-        std::is_class<pure_t<T>>::value
-      , decltype(factory::functor(object, &pure_t<T>::operator()))
-    >::type
-    {
-        return factory::functor(
-            std::forward<decltype(object)>(object)
-          , &pure_t<T>::operator()
-        );
-    }
+    template <typename R, typename ...P> functor_t(R(*)(P...)) -> functor_t<R(P...)>;
+    template <typename R, typename ...P> functor_t(R(*)(P..., ...)) -> functor_t<R(P..., ...)>;
+    template <typename F, typename O> functor_t(F O::*) -> functor_t<F, O>;
 }
 
 MUSEQA_END_NAMESPACE
