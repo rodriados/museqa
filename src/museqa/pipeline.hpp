6811144f2009ae6cd84d5f43b47d0e0378de6942
Rodrigo Siqueira <rodriados@gmail.com>
2024-11-04 00:05:10 -0400

    [pipeline] Definition of pipeline context and runner

--- src/museqa/pipeline.hpp
+++ src/museqa/pipeline.hpp
@@ -21,36 +21,39 @@ MUSEQA_BEGIN_NAMESPACE
 namespace pipeline
 {
     /**
-     * The state of a pipeline is a generic container that carries stateful data
+     * The context of a pipeline is a generic container that carries stateful data
      * between different modules and middlewares during the execution of the pipeline.
      * @since 1.0
      */
-    class state_t
+    class context_t
     {
         private:
             typedef memory::pointer::shared_t<void> entry_t;
-            typedef std::unordered_map<std::string, entry_t> context_t;
+            typedef std::unordered_map<std::string, entry_t> state_t;
 
-        public:
+        private:
             /**
-             * The representation of a state key, which is used externally to search
-             * for an entry within the state date. The key joins the data type with
-             * its corresponding entry id.
+             * The representation of a context key, which is used externally to
+             * search for an entry within the context date. The key joins the data
+             * type with its corresponding entry id.
+             * @tparam T The type of data represented by the key.
              * @since 1.0
              */
             template <typename T>
-            struct statekey_t { const char *id = nullptr; };
+            struct key_t { const char *id = nullptr; };
+
+        template <typename T> friend MUSEQA_CONSTEXPR const key_t<T> key(const char*) noexcept;
 
         private:
-            context_t m_context = {};
+            state_t m_state = {};
 
         public:
-            inline explicit state_t() = default;
-            inline explicit state_t(const state_t&) = default;
-            inline explicit state_t(state_t&&) = default;
+            MUSEQA_INLINE explicit context_t() = default;
+            MUSEQA_INLINE explicit context_t(const context_t&) = default;
+            MUSEQA_INLINE explicit context_t(context_t&&) = default;
 
-            inline state_t& operator=(const state_t&) = default;
-            inline state_t& operator=(state_t&&) = default;
+            MUSEQA_INLINE context_t& operator=(const context_t&) = default;
+            MUSEQA_INLINE context_t& operator=(context_t&&) = default;
 
             /**
              * Retrieves data referenced by the given key from the context.
@@ -59,9 +62,9 @@ namespace pipeline
              * @return The data retrieved from the context.
              */
             template <typename T>
-            inline memory::pointer::shared_t<T>& get(const statekey_t<T>& key)
+            MUSEQA_INLINE memory::pointer::shared_t<T>& get(const key_t<T>& key)
             {
-                return reinterpret_cast<memory::pointer::shared_t<T>&>(m_context[key.id]);
+                return reinterpret_cast<memory::pointer::shared_t<T>&>(m_state[key.id]);
             }
 
             /**
@@ -71,36 +74,36 @@ namespace pipeline
              * @param value The value to assign to the key in the context.
              */
             template <typename T>
-            inline void set(const statekey_t<T>& key, memory::pointer::shared_t<T>& value)
+            MUSEQA_INLINE void set(const key_t<T>& key, memory::pointer::shared_t<T>& value)
             {
-                m_context.insert_or_assign(key.id, reinterpret_cast<entry_t&>(value));
+                m_state.insert_or_assign(key.id, reinterpret_cast<entry_t&>(value));
             }
 
             /**
              * Removes data referenced by a given key from the context. No errors
              * or exceptions will be raised if key is not found in the context.
-             * @tparam T The type of data to be removed from state.
+             * @tparam T The type of data to be removed from context.
              * @param key The key to be removed from the context.
              * @return Has the key been found and removed?
              */
             template <typename T>
-            inline bool remove(const statekey_t<T>& key)
+            MUSEQA_INLINE bool remove(const key_t<T>& key)
             {
-                return (bool) m_context.erase(key.id);
+                return (bool) m_state.erase(key.id);
             }
     };
 
     /**
-     * Creates a new pipeline state key from its type and id. This function serves
-     * as a shortcut to easen a pipeline state key creation.
-     * @tparam T The type attached to the pipeline state key.
+     * Creates a new pipeline context key from its type and id. This function serves
+     * as a shortcut to easen a pipeline context key creation.
+     * @tparam T The type attached to the pipeline context key.
      * @param id The key string identification.
-     * @return The new pipeline state key instance.
+     * @return The new pipeline context key instance.
      */
-    template <typename T = void>
-    inline constexpr auto key(const char *id) noexcept -> const state_t::statekey_t<T>
+    template <typename T>
+    MUSEQA_CONSTEXPR const context_t::key_t<T> key(const char *id) noexcept
     {
-        return state_t::statekey_t<T> {id};
+        return context_t::key_t<T> {id};
     }
 
     /**
@@ -108,7 +111,7 @@ namespace pipeline
      * and middlewares down the execution chain of a pipeline.
      * @since 1.0
      */
-    using pipe_t = memory::pointer::shared_t<state_t>;
+    using pipe_t = memory::pointer::shared_t<context_t>;
 
     /**
      * The abstract base of a pipeline module. Modules are chained in a pipeline
@@ -123,23 +126,23 @@ namespace pipeline
         /**
          * The method to run before the module's core logic. If needed, a module
          * can perform validations or initialize state before before the core logic.
-         * @param pipe The pipeline's transitive state instance.
+         * @param pipe The pipeline's transitive context instance.
          */
-        virtual void before(pipe_t& pipe) const { }
+        virtual void before(pipe_t& pipe) const {}
 
         /**
          * The method to execute the module's core logic. After the pipeline has
          * been successfully initialized, the module's core logic is can be run.
-         * @param pipe The pipeline's transitive state instance.
+         * @param pipe The pipeline's transitive context instance.
          */
         virtual void run(pipe_t& pipe) const = 0;
 
         /**
          * The method to run after the module's core logic. Within a pipeline, modules
          * have this method called in the opposite order to which they are defined.
-         * @param pipe The pipeline's transitive state instance.
+         * @param pipe The pipeline's transitive context instance.
          */
-        virtual void after(pipe_t& pipe) const { }
+        virtual void after(pipe_t& pipe) const {}
     };
 
     /**
@@ -160,33 +163,33 @@ namespace pipeline
             const sequence_t m_sequence = {};
 
         static_assert(
-            utility::all(std::is_base_of<pipeline::module_t, pure_t<M>>::value...)
+            utility::all(std::is_base_of_v<pipeline::module_t, pure_t<M>>...)
           , "every pipeline step must ultimately inherit from a module"
         );
 
         public:
-            inline constexpr middleware_t() = default;
-            inline constexpr middleware_t(const middleware_t&) = default;
-            inline constexpr middleware_t(middleware_t&&) = delete;
+            MUSEQA_CONSTEXPR middleware_t() = default;
+            MUSEQA_CONSTEXPR middleware_t(const middleware_t&) = default;
+            MUSEQA_CONSTEXPR middleware_t(middleware_t&&) = delete;
 
             /**
              * Configures a new pipeline middleware with the modules it must execute.
              * @param modules The middleware's modules instance list.
              */
-            inline constexpr middleware_t(M&&... modules)
+            MUSEQA_CONSTEXPR middleware_t(M&&... modules)
               : m_sequence (std::forward<decltype(modules)>(modules)...)
             {}
 
-            inline middleware_t& operator=(const middleware_t&) = delete;
-            inline middleware_t& operator=(middleware_t&&) = delete;
+            MUSEQA_INLINE middleware_t& operator=(const middleware_t&) = delete;
+            MUSEQA_INLINE middleware_t& operator=(middleware_t&&) = delete;
 
             /**
              * Executes the validation or initialization logic of all modules referenced
              * by the middleware. Note that they are guaranteed to be called in
              * the order in which they are declared.
-             * @param pipe The pipeline's transitive state instance.
+             * @param pipe The pipeline's transitive context instance.
              */
-            inline virtual void before(pipe_t& pipe) const override
+            MUSEQA_INLINE virtual void before(pipe_t& pipe) const override
             {
                 supertuple::foreach(m_sequence, &module_t::before, pipe);
             }
@@ -195,9 +198,9 @@ namespace pipeline
              * Executes the core logic of all modules referenced by the middleware.
              * Note that they are guaranteed to be called in the order in which
              * the modules are declared.
-             * @param pipe The pipeline's transitive state instance.
+             * @param pipe The pipeline's transitive context instance.
              */
-            inline virtual void run(pipe_t& pipe) const override
+            MUSEQA_INLINE virtual void run(pipe_t& pipe) const override
             {
                 supertuple::foreach(m_sequence, &module_t::run, pipe);
             }
@@ -206,9 +209,9 @@ namespace pipeline
              * Executes the finalization logic of all modules referenced by the
              * middleware. Note that, differently from the previous steps, this
              * method guarantees that modules are called in opposite order.
-             * @param pipe The pipeline's transitive state instance.
+             * @param pipe The pipeline's transitive context instance.
              */
-            inline virtual void after(pipe_t& pipe) const override
+            MUSEQA_INLINE virtual void after(pipe_t& pipe) const override
             {
                 supertuple::rforeach(m_sequence, &module_t::after, pipe);
             }
@@ -231,36 +234,36 @@ namespace pipeline
             const executor_t m_executor = {};
 
         public:
-            inline constexpr runner_t() = default;
-            inline constexpr runner_t(const runner_t&) = default;
-            inline constexpr runner_t(runner_t&&) = delete;
+            MUSEQA_CONSTEXPR runner_t() = default;
+            MUSEQA_CONSTEXPR runner_t(const runner_t&) = default;
+            MUSEQA_CONSTEXPR runner_t(runner_t&&) = delete;
 
             /**
              * Configures a new pipeline runner with the modules it must execute.
              * @param modules The runner's modules instance list.
              */
-            inline constexpr runner_t(M&&... modules)
+            MUSEQA_CONSTEXPR runner_t(M&&... modules)
               : m_executor (std::forward<decltype(modules)>(modules)...)
             {}
 
-            inline runner_t& operator=(const runner_t&) = delete;
-            inline runner_t& operator=(runner_t&&) = delete;
+            MUSEQA_INLINE runner_t& operator=(const runner_t&) = delete;
+            MUSEQA_INLINE runner_t& operator=(runner_t&&) = delete;
 
             /**
              * Runs the pipeline and returns the resulting objects.
-             * @param pipe An optional pipeline's transitive state instance.
-             * @return The resulting pipeline state.
+             * @param pipe An optional pipeline's transitive context instance.
+             * @return The resulting pipeline context.
              */
-            inline auto run(const pipe_t& pipe = {}) const -> pipe_t
+            MUSEQA_INLINE pipe_t run(const pipe_t& pipe = {}) const
             {
-                pipe_t state = pipe ? pipe :
-                    factory::memory::pointer::shared<state_t>();
+                pipe_t context = pipe ? pipe :
+                    factory::memory::pointer::shared<context_t>();
 
-                m_executor.before(state);
-                m_executor.run(state);
-                m_executor.after(state);
+                m_executor.before(context);
+                m_executor.run(context);
+                m_executor.after(context);
 
-                return state;
+                return context;
             }
     };
 
@@ -278,10 +281,10 @@ namespace factory::pipeline
      * Initializes a new pipeline state pipe.
      * @return The new pipeline state pipe instance.
      */
-    inline auto pipe() -> museqa::pipeline::pipe_t
+    MUSEQA_INLINE museqa::pipeline::pipe_t pipe()
     {
-        using state_t = museqa::pipeline::state_t;
-        return factory::memory::pointer::shared<state_t>();
+        using context_t = museqa::pipeline::context_t;
+        return factory::memory::pointer::shared<context_t>();
     }
 }
 
