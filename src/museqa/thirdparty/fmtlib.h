9ada44b3ff20aed8544c5e9a31472d69f6e74008
Rodrigo Siqueira <rodriados@gmail.com>
2024-11-10 01:10:49 -0400

    [fmtlib] Avoid warnings when compiling with NVCC

--- src/museqa/thirdparty/fmtlib.h
+++ src/museqa/thirdparty/fmtlib.h
@@ -9,8 +9,10 @@
 #include <museqa/environment.h>
 
 #if !defined(MUSEQA_AVOID_FMTLIB)
-  MUSEQA_DISABLE_NVCC_WARNING_BEGIN(unrecognized_gcc_pragma)
+  MUSEQA_DISABLE_NVCC_WARNING_BEGIN(128)
+  MUSEQA_DISABLE_NVCC_WARNING_BEGIN(2417)
     #include <fmt/format.h>
     #include <fmt/ranges.h>
-  MUSEQA_DISABLE_NVCC_WARNING_END(unrecognized_gcc_pragma)
+  MUSEQA_DISABLE_NVCC_WARNING_END(2417)
+  MUSEQA_DISABLE_NVCC_WARNING_END(128)
 #endif
